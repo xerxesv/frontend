b1b34adbff89c218cbffb147f24b25492d461a8b
Anton Solomonik <xerxesv@gmail.com>
2015-12-19 19:31:06 +0000

    some front end components

---
+++ notes.js
@@ -0,0 +1,27 @@
+// takes an array of values
+// when the user starts typing something in, 
+  // if the input is not empty, takes in the current input
+  // finds the elements in the array that match that input
+  // makes the ul below visible
+  // populates the ul with data
+
+// bind events to the input:
+  // input
+    // when the user starts typing something in, 
+      // if the input is not empty, takes in the current input
+      // finds the elements in the array that match that input
+      // makes the ul below visible
+      // populates the ul with data
+
+  // blur
+    // when it's unselected hide the list
+
+  // keydown
+    // down travels down
+    // up travels up
+    // enter 'selects' the element (makes it the value of the field)
+
+
+// bind events to the list
+  // mousedown
+
